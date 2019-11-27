649fc6dbbb6a59b51befd35ae2dbc464715819f8
icecreammikan <ryrh31223458@gmail.com>
2019-11-27 14:28:14 +0900

    commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,17 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def new
+    @books = Book.new
+  end
+
+  def create
+    Book.create(book_params)
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:name, :image, :text)
+  end
 end

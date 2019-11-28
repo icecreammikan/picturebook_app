cd5faa13824280753487a3392d97c3e0566b063d
icecreammikan <ryrh31223458@gmail.com>
2019-11-28 16:23:01 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  # get 'books/index'
+  post 'books/index'
   post 'books/new'
-  # get 'books/create'
-  # get 'books/show'
+  post 'books/create'
+  post 'books/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "books#index"
   resources :books, only: [:index, :new, :create, :show]
